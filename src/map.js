0ab56f3d55f5a09c71f8008ecb1526ec10d30098
Jeremy <jeremy.geneste@gmail.com>
2019-07-28 13:53:20 +0200

    [ FEATURE ] Customized Google Map Markers

--- src/map.js
+++ src/map.js
@@ -2,8 +2,13 @@ let map;
 // On initialise la Google Map
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
+    center: { lat: 48.855262, lng: 2.346055 },
+    zoom: 2.5
+  });
+  const marker = new google.maps.Marker({
+    position: { lat: 48.855262, lng: 2.346055 },
+    map: map,
+    icon: 'assets/marker.png'
   });
 }
 
